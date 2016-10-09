10502f4d7e46cd49c640fb081a399fd6a10633c4
snuffykl <adri4n7@gmail.com>
2016-10-09 16:24:37 +0800

    Add authors with react lifecycle

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+	authors: 
+	[
+		{
+			id: 'snuffykl', 
+			firstName: 'Adrian', 
+			lastName: 'SnuffyKL'
+		},	
+		{
+			id: 'ginni', 
+			firstName: 'Ginni', 
+			lastName: 'Snail'
+		},	
+		{
+			id: 'spykix', 
+			firstName: 'Brian', 
+			lastName: 'Spykix'
+		}
+	]
+};
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Home = require('./components/home');
+var Authors = require('./components/authors/authorPage');
 var About = require('./components/about/aboutPage');
 var Header = require('./components/common/header');
 
@@ -15,7 +16,8 @@ var App = React.createClass({
 
 		switch(this.props.route){
 			case 'about': Child = About; break;
-			default: Child = Home;
+			case 'authors': Child = Authors; break;
+			default: Child = Home; break;
 		}
 
 		return (
